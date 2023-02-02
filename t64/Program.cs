8a78c9bdd2c5ddcbab252d8194a01ff97d5ff967
Ilnurios325 <ipilulka@gmail.com>
2023-02-03 00:14:03 +0300

    resolve t64

---
+++ t64/Program.cs
@@ -0,0 +1,10 @@
+﻿// 64. Задайте N. Выведите все значения от N до 1 через рекурсию.
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+string Numbers(int a, int b)
+    {
+      return a<=b ? Numbers(a+1, b) + $"{a} " : String.Empty;  
+    }
+    Console.WriteLine(Numbers(a,b)); 
